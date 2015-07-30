d53fa50efae32ce5e867730b4aca436135d7f6b5
Florian Albrecht <Florian.Albrecht@hamburgsud.com>
2015-07-30 11:27:59 +0200

    Added required license headers

--- src/main/java/org/aludratest/service/database/tablecolumn/IntColumn.java
+++ src/main/java/org/aludratest/service/database/tablecolumn/IntColumn.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2014 Hamburg Sud and the contributors.
+ * Copyright (C) 2015 Hamburg Sud and the contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
